21580666a7a397f7eb87e01b6103b3fc1a2d35b3
Ellen Kramp <ellenkramp@Ellens-MacBook-Pro.local>
2018-03-07 16:07:13 -0500

    setting up layout

---
+++ javascript/detail.js
@@ -0,0 +1,5 @@
+// alert('connected');
+
+var userImage = document.getElementById('user-image');
+var userImageSource = "http://www.unit2fitness.com/wp-content/uploads/2013/01/Graffiti-Wallpaper-027.jpg";
+userImage.src = userImageSource;
