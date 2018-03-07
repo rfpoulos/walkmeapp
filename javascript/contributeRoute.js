9e186a5bd700135b15f4f3cb500ddfc85e3d0a2b
rfpoulos <rfpoulos@outlook.com>
2018-03-07 15:30:55 -0500

    the form now stores data in a local array as an object

--- javascript/contributeRoute.js
+++ javascript/contributeRoute.js
@@ -1,1 +1,26 @@
-console.log('testing connection');
+var walkForm = document.querySelector("[data-walk='form']");
+var userID = document.querySelector("[name='user']");
+var title = document.querySelector("[name='contributer_title']");
+var address = document.querySelector("[name='start_address']");
+var city = document.querySelector("[name='walk_city']");
+var state = document.querySelector("[name='walk_state']");
+var description = document.querySelector("[name='contributer_description']");
+var thumbnail = document.querySelector("[name='contributer_thumbnail']");
+var localWalkArray = [];
+
+
+var recordWalk = function(event) {
+    event.preventDefault();
+    var currentWalk = {
+        "userID": userID.value,
+        "title": title.value,
+        "address": address.value,
+        "city": city.value,
+        "state": state.value,
+        "description": description.value,
+        "thumbnail": thumbnail.value
+    };
+    localWalkArray.push(currentWalk);
+    console.log(localWalkArray);
+};
+walkForm.addEventListener("submit", recordWalk);
