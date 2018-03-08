ddfca5f4a853bff7e618acc067cf06ea276164d6
rfpoulos <rfpoulos@outlook.com>
2018-03-08 15:05:13 -0500

    adds POIs to routes an updates firebase

--- javascript/contributeRoute.js
+++ javascript/contributeRoute.js
@@ -1,5 +1,7 @@
 var fireBaseRef = firebase.database().ref('routes');
 var walkForm = document.querySelector("[data-walk='form']");
+var poiForm = document.querySelector("[data-poi='form']");
+var poiAddress = document.querySelector("[name='next-poi']");
 var userID = document.querySelector("[name='user']");
 var title = document.querySelector("[name='contributer_title']");
 var address = document.querySelector("[name='start_address']");
@@ -7,23 +9,34 @@ var city = document.querySelector("[name='walk_city']");
 var state = document.querySelector("[name='walk_state']");
 var description = document.querySelector("[name='contributer_description']");
 var thumbnail = document.querySelector("[name='contributer_thumbnail']");
-var mapContainer = document.querySelector(".map")
-
+var mapContainer = document.querySelector(".map");
+var map;
+var localRoute;
+var localRouteRef;
 
 var geoURL = function(str, cty, ste) {
-    var geoURLResponse = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + str.value + ',' + cty.value + ',' + ste.value + '&key=AIzaSyDDFmtGsZQUblhJEuXo9YNrN6pFO_tfiW0';
+    var geoURLResponse = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + str + ',' + cty + ',' + ste + '&key=AIzaSyDDFmtGsZQUblhJEuXo9YNrN6pFO_tfiW0';
     return geoURLResponse;
 }
 
 var initMap = function(location, zoomLevel) {
-    console.log("I ran! ... so far away")
-    var map = new google.maps.Map(mapContainer, {
+    map = new google.maps.Map(mapContainer, {
         center: location,
         zoom: zoomLevel
-});
+    });
+    addPOIMarker(location, map);
 }
-var getGeoLocation = function(URL) {
-    return fetch(URL, {
+
+var addPOIMarker = function(poi, map) {
+    var marker = new google.maps.Marker({
+        position: poi,
+        map: map
+    });
+    return marker
+}
+
+var getGeoLocation = function(url) {
+    return fetch(url, {
     })
     .then(function(response){
         return response.json();
@@ -36,12 +49,22 @@ var getGeoLocation = function(URL) {
     })
 }
 
+var addPOI = function(event) {
+    event.preventDefault();
+    var markerLocation = getGeoLocation(geoURL(poiAddress, localRoute['city'], localRoute['state']));
+    markerLocation.then(function(data){
+        addPOIMarker(data, map);
+        localRoute['pois'].push(data);
+        localRouteRef.set(localRoute);
+        poiForm.reset();
+    });
+}
+
 var recordWalk = function(event) {
     event.preventDefault();
-    console.log(geoURL(address, city, state));
-    var mapLocation = getGeoLocation(geoURL(address, city, state));
+    var mapLocation = getGeoLocation(geoURL(address.value, city.value, state.value));
     mapLocation.then(function(data){
-        initMap(data, 17);
+        initMap(data, 15);
         var currentWalk = {
             "userID": userID.value,
             "title": title.value,
@@ -50,11 +73,16 @@ var recordWalk = function(event) {
             "state": state.value,
             "description": description.value,
             "thumbnail": thumbnail.value,
-            "startLocation": data
+            "startLocation": data,
+            "pois": [data]
         };
-        fireBaseRef.push().set(currentWalk);
+        var walkObject = fireBaseRef.push()
+        walkObject.set(currentWalk);
+        localRouteRef = walkObject;
+        localRoute = currentWalk;
         walkForm.reset();
     })
 };
 
 walkForm.addEventListener("submit", recordWalk);
+poiForm.addEventListener("submit", addPOI);
