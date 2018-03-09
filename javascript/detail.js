c9bbd6f3f4924ad1f7939872f1d470509d1b9aba
Ellen Kramp <ellenkramp@Ellens-MacBook-Pro.local>
2018-03-09 13:37:41 -0500

    added media query to adjust styling for screens below 600px wide

--- javascript/detail.js
+++ javascript/detail.js
@@ -1,16 +1,54 @@
 // alert('connected');
 var fireBaseRef = firebase.database().ref('routes');
-var docAddress = document.getElementById('address');
-var objectId = '-L763yP2Gv0-nU4jlWAp';
+var objectId = '-L7AaTEf_384qFSsm-OQ';
+var fireBaseDescRef = firebase.database().ref('routes/' + objectId + '/description');
 var fireBaseAddressRef = firebase.database().ref('routes/' + objectId + '/address');
-var fbPath = fireBaseRef + 
+var fireBaseCityRef = firebase.database().ref('routes/' + objectId + '/city');
+var fireBaseStateRef = firebase.database().ref('routes/' + objectId + '/state');
+var fireBaseTitleRef = firebase.database().ref('routes/' + objectId + '/title');
+var fireBaseUserIdRef = firebase.database().ref('routes/' + objectId + '/userID');
+var fireBaseThumbnailRef = firebase.database().ref('routes/' + objectId + '/thumbnail');
+
+var docTitle = document.getElementById('title');
+fireBaseTitleRef.on('value', function(snapshot) {
+    docTitle.textContent = snapshot.val();
+});
+
+var docDesc = document.getElementById('description');
+fireBaseDescRef.on('value', function(snapshot) {
+    docDesc.textContent = snapshot.val();
+});
+
+var docAddress = document.getElementById('address');
 fireBaseAddressRef.on('value', function(snapshot) {
     docAddress.textContent = snapshot.val();
 });
 
-var userImage = document.getElementById('user-image');
-var userImageSource = "http://www.unit2fitness.com/wp-content/uploads/2013/01/Graffiti-Wallpaper-027.jpg";
-userImage.src = userImageSource;
+var docCity = document.getElementById('city');
+fireBaseCityRef.on('value', function(snapshot) {
+    docCity.textContent = snapshot.val();
+});
 
-var mapImg = document.getElementById('mapImg');
-mapImg.src = 'https://www.google.com/permissions/images/maps-att.png';
+var docState = document.getElementById('state');
+fireBaseStateRef.on('value', function(snapshot) {
+    docState.textContent = snapshot.val();
+});
+
+var docUserId = document.getElementById('userId');
+fireBaseUserIdRef.on('value', function(snapshot) {
+    docUserId.textContent = snapshot.val();
+});
+
+var userImage = document.getElementById('user-image');
+fireBaseThumbnailRef.on('value', function(snapshot) {
+    userImage.src = snapshot.val();
+});
+
+var docMap = document.querySelector(".map");
+var map;
+
+var initMap = function(location, zoomLevel) {
+    map = new google.maps.Map(mapContainer, {
+        center: location,
+        zoom: zoomLevel
+})};
