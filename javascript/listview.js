dd4b424ddb14a1880a0758aaf8b6e52c35606d2c
Aaron Gross <aarongross@MacBook-Pro.local>
2018-03-09 14:13:22 -0500

    took out comments

--- javascript/listview.js
+++ javascript/listview.js
@@ -1,34 +1,3 @@
-// alert('connected');
-// var fireBaseRef = firebase.database().ref('routes');
-// var docAddress = document.getElementById('address');
-// var objectId = '-L763yP2Gv0-nU4jlWAp';
-// var fireBaseAddressRef = firebase.database().ref('routes/' + objectId + '/address');
-// var fbPath = fireBaseRef + 
-// fireBaseAddressRef.on('value', function(snapshot) {
-//     docAddress.textContent = snapshot.val();
-// });
-
-// var userImage = document.getElementById('user-image');
-// var userImageSource = "http://www.unit2fitness.com/wp-content/uploads/2013/01/Graffiti-Wallpaper-027.jpg";
-// userImage.src = userImageSource;
-
-// var mapImg = document.getElementById('mapImg');
-// mapImg.src = 'https://www.google.com/permissions/images/maps-att.png';
-
-// var thumbnail
-// var favorite
-// var title
-// var stars
-// var number of reviews
-
-// var address
-// var city
-// var state
-
-// var routeLength
-// var routeTime
-
-// var userID
 
 //Creates route-container div
 var body = document.querySelector('body')
