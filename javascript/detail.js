54e09f9e68f805b4e3e0198280b625fc6e84c9ed
rfpoulos <rfpoulos@outlook.com>
2018-03-11 15:06:22 -0400

    some styling to detail buttons and map

--- javascript/detail.js
+++ javascript/detail.js
@@ -39,11 +39,11 @@ var addPOIMarker = function(poi, poiTitle, poiContent) {
     });
     var poiContainer = document.createElement('div');
 
-    var contentDiv = document.createElement('div');
+    var contentDiv = document.createElement('h4');
     contentDiv.classList.add('poi-content');
     contentDiv.textContent = poiContent;
 
-    var titleDiv = document.createElement('div');
+    var titleDiv = document.createElement('h2');
     titleDiv.classList.add('poi-title');
     titleDiv.textContent = poiTitle;
 
