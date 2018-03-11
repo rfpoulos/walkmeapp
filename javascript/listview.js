3b0da159e802a23b975b5e219fc099090f727570
Aaron Gross <aarongross@MacBook-Pro.local>
2018-03-11 13:23:07 -0400

    dynamic card...no server info yet

--- javascript/listview.js
+++ javascript/listview.js
@@ -54,7 +54,7 @@ infoSectionSelector.appendChild(divContributer);
 var titleAndDistanceSelector = document.querySelector('.title-and-distancefrom');
 var divRouteTitle = document.createElement('div');
 divRouteTitle.setAttribute("class", "route-title");
-divRouteTitle.textContent = 'Graffiti'
+divRouteTitle.textContent = 'Graffiti';
 titleAndDistanceSelector.appendChild(divRouteTitle);
 
 var divDistanceFrom = document.createElement('div');
@@ -63,7 +63,7 @@ var divDistanceIcon = document.createElement('div');
 divDistanceIcon.setAttribute("class", "distancefrom-icon");
 var divDistanceNumber = document.createElement('div');
 divDistanceNumber.setAttribute("class", "distancefrom-number");
-divDistanceNumber.textContent = '1.2 mi'
+divDistanceNumber.textContent = '1.2 mi';
 var imgDistanceIcon = document.createElement('img');
 imgDistanceIcon.setAttribute("class", "nearme");
 imgDistanceIcon.setAttribute("src", "images/nearme.png");
@@ -74,6 +74,53 @@ divDistanceFrom.appendChild(divDistanceNumber);
 divDistanceIcon.appendChild(imgDistanceIcon);
 
 // Info Section - Review Row
+var divReviewStars = document.createElement('div');
+divReviewStars.setAttribute("class", "review-stars");
+var imgReviewStars = document.createElement('img');
+imgReviewStars.setAttribute('src', 'images/fullstar.png');
+imgReviewStars.setAttribute('class', 'stars');
+divReviews.appendChild(divReviewStars);
+divReviewStars.appendChild(imgReviewStars);
+
+
+var divNumberOfReviews = document.createElement('div');
+divNumberOfReviews.setAttribute("class", "number-of-reviews");
+divNumberOfReviews.textContent = "32 Reviews";
+divReviews.appendChild(divNumberOfReviews);
+
+// Info Section - Address Row
+var imgPlaceIcon = document.createElement('img');
+imgPlaceIcon.setAttribute('src', 'images/place_small.png');
+imgPlaceIcon.setAttribute('class', 'place-icon');
+divAddress.appendChild(imgPlaceIcon);
+
+var divAddressInfo = document.createElement('div');
+divAddressInfo.setAttribute('class', 'address-info')
+divAddressInfo.textContent = "206 Rogers St NE \r\n";
+divAddressInfo.textContent += "Atlanta, GA"
+divAddress.appendChild(divAddressInfo)
+
+// Info Section - Route Time and Length Row
+var imgRouteIcon = document.createElement('img');
+imgRouteIcon.setAttribute('src', 'images/route_small.png');
+imgRouteIcon.setAttribute('class', 'route-icon');
+divRouteTimeAndLength.appendChild(imgRouteIcon);
+
+var divTimeAndLengthInfo = document.createElement('div');
+divTimeAndLengthInfo.setAttribute('class', 'time-and-length-info');
+divTimeAndLengthInfo.textContent = "23 mins / 1.4 mi";
+divRouteTimeAndLength.appendChild(divTimeAndLengthInfo);
+
+// Info Section - Contributer Row
+var imgContributerIcon = document.createElement('img');
+imgContributerIcon.setAttribute('src', 'images/contributer_small.png');
+imgContributerIcon.setAttribute('class', 'contributer-icon');
+divContributer.appendChild(imgContributerIcon);
+
+var divContributerName = document.createElement('div');
+divContributerName.setAttribute('class', 'contributer-name');
+divContributerName.textContent = 'Ponchieponcho';
+divContributer.appendChild(divContributerName);
 
 
 
